2b9277d7b400914fcdb292dbeeceac85890a28c3
zhangshuai <zhangshuaioop>
2019-03-13 11:47:15 +0800

    zhangshuai

---
+++ src/main/java/com/example/timemachine/TimeMachineApplication.java
@@ -0,0 +1,12 @@
+package com.example.timemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeMachineApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/timemachine/TimeMachineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.timemachine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeMachineApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
